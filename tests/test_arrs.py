6dbaa07bef51c8992d6681015b4ee2c6231b0230
DaniilKokorev <kokorevdaniil@mail.ru>
2023-12-31 16:44:21 +0700

    issue2_test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,6 +3,11 @@ import pytest
 from utils import arrs
 
 
+@pytest.fixture
+def num_list():
+    return [1, 2, 3, 4]
+
+
 def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
     assert arrs.get([], 0, "test") == "test"
@@ -17,5 +22,5 @@ def test_slice_coll_zero():
     assert arrs.my_slice([]) == []
 
 
-def test_slice_start_zero():
-    assert arrs.my_slice([1, 2, 3]) == [1, 2, 3]
+def test_slice_start_zero(num_list):
+    assert arrs.my_slice(num_list) == [1, 2, 3, 4]
